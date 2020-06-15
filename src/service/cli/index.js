fdd287ae11c2eee84bb6cf01789aa9ad3ac462be
Vladimir Mazo <vladimir.mazo@icl-services.com>
2020-06-15 10:51:26 +0300

    Added server.js logic

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -4,19 +4,6 @@ const help = require(`./help`);
 const generate = require(`./generate`);
 const version = require(`./version`);
 const server = require(`./server`);
-const {getLogger} = require(`../logger`);
-const logger = getLogger();
-const sequelize = require(`../db`);
-
-const checkDatabase = async () => {
-  try {
-    await sequelize.authenticate();
-    logger.info(`Connected to database successfully!`);
-  } catch (e) {
-    logger.error(`Failed connect to database: ${e}`);
-    process.exit(1);
-  }
-};
 
 const Cli = {
   [generate.name]: generate,
@@ -26,6 +13,5 @@ const Cli = {
 };
 
 module.exports = {
-  Cli,
-  checkDatabase
+  Cli
 };
